8fc11f78e663e1b84fa92dc7b83ef174f4cc99de
raymon <raymon.guo@outlook.com>
2019-12-03 15:15:32 +0800

    打家劫舍-使用函数测试

--- src/house_robber.rs
+++ src/house_robber.rs
@@ -1,16 +1,17 @@
 use std::cmp::max;
 
+#[test]
+fn test() {
+    assert_eq!(Solution::rob(vec![]), 0);
+    assert_eq!(Solution::rob(vec![1]), 1);
+    assert_eq!(Solution::rob(vec![2, 1]), 2);
+    assert_eq!(Solution::rob(vec![1, 2, 3, 1]), 4);
+    assert_eq!(Solution::rob(vec![2, 7, 9, 3, 1]), 12);
+}
+
 pub struct Solution {}
 
 impl Solution {
-    pub fn test() {
-        assert_eq!(Solution::rob(vec![]), 0);
-        assert_eq!(Solution::rob(vec![1]), 1);
-        assert_eq!(Solution::rob(vec![2, 1]), 2);
-        assert_eq!(Solution::rob(vec![1, 2, 3, 1]), 4);
-        assert_eq!(Solution::rob(vec![2, 7, 9, 3, 1]), 12);
-    }
-
     /** 动态规划
     状态初始定义：dp[0] = nums[0], dp[1] = max(nums[0], nums[1])
     状态转义方程：dp[n] = max(nums[n] + dp[n-2], dp[n-1])
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
 mod house_robber;
-use chrono::prelude::*;
-use house_robber::Solution;
 
-fn main() {
-    let start = Local::now().timestamp_nanos();
-    Solution::test();
-    let end = Local::now().timestamp_nanos();
-
-    println!(
-        "done! start: {:?}, end: {:?}, duration: {:?}",
-        start,
-        end,
-        end - start
-    );
-}
+fn main() {}
