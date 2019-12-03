5ef3bf9011f45cb9655a65fe8d939776c3d921f3
raymon <raymon.guo@outlook.com>
2019-12-03 10:35:06 +0800

    打家劫舍-断言，算法优化

--- src/house_robber.rs
+++ src/house_robber.rs
@@ -3,6 +3,14 @@ use std::cmp::max;
 pub struct Solution {}
 
 impl Solution {
+    pub fn test() {
+        assert_eq!(Solution::rob(vec![]), 0);
+        assert_eq!(Solution::rob(vec![1]), 1);
+        assert_eq!(Solution::rob(vec![2, 1]), 2);
+        assert_eq!(Solution::rob(vec![1, 2, 3, 1]), 4);
+        assert_eq!(Solution::rob(vec![2, 7, 9, 3, 1]), 12);
+    }
+
     /** 动态规划
     状态初始定义：dp[0] = nums[0], dp[1] = max(nums[0], nums[1])
     状态转义方程：dp[n] = max(nums[n] + dp[n-2], dp[n-1])
@@ -13,20 +21,21 @@ impl Solution {
             return 0;
         }
 
+        if len == 1 {
+            return nums[0];
+        }
+
+        if len == 2 {
+            return max(nums[0], nums[1]);
+        }
+
         let mut dp = vec![0; max(2, nums.len())];
-        if len >= 1 {
-            dp[0] = nums[0]
-        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        if len >= 2 {
-            dp[1] = max(nums[0], nums[1])
-        }
-
-        let mut res = max(dp[0], dp[1]);
         for i in 2..dp.len() {
             dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
-            res = max(res, dp[i]);
         }
-        res
+        dp[len - 1]
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod house_robber;
+use chrono::prelude::*;
 use house_robber::Solution;
 
 fn main() {
-    let data = vec![0, 1];
-    println!("{}", Solution::rob(data));
+    let start = Local::now().timestamp_nanos();
+    Solution::test();
+    let end = Local::now().timestamp_nanos();
+
+    println!(
+        "done! start: {:?}, end: {:?}, duration: {:?}",
+        start,
+        end,
+        end - start
+    );
 }
